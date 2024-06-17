dacb2fcb6a2f0ed104200805bbe1470e09c9d751
abhishek singh <singhsabhishek23@gmail.com>
2024-06-17 15:58:29 +0530

    set

---
+++ object/set.js
@@ -0,0 +1,21 @@
+// javascript set ---  in javascript , a set is a build in object that allow you to store a unique values of
+//  any type . this means that a set contain only distinct value , and any duplicate automattically ignored.
+
+
+// this is create by three type
+
+//  1. using the 'new' keyword with the set constructor
+
+//  const myset = new set()
+
+// 2.  using array method
+
+// const fruitsarray = ['apple' , 'banana' , 'grapes']
+// const fruitset = new set(fruitsarray)
+
+// 3. using set literal
+
+// const myset = new set(['apple','banana'])
+
+
+
---
+++ object/setmethod.js
@@ -0,0 +1,47 @@
+// adding and remove element 
+
+// const myset = new Set()
+// myset.add('apple')
+// myset.add('banana')
+// myset.add('grapes')
+// myset.add('apple')
+
+// // console.log(typeof(myset))
+
+// console.log(myset)
+
+// myset.delete('apple')
+
+// console.log(myset)
+
+// myset.clear()
+
+// console.log(myset)
+
+
+//   check if element is in the set
+
+// const myset = new Set(['apple' , 'banana' , 'grapes' , 'orange'])
+// console.log(myset.has('papaya'))
+// console.log(myset.has('apple'))
+
+// find size of set
+
+// const myset = new Set(['apple' , 'banana' , 'orange' , 'apple'])
+// console.log(myset.size)
+
+
+//iteration through a set
+
+// 1..using foreach loop
+
+ //const myset = new Set(['apple' , 'banana' , 'papaya' , 'grapes'])
+// myset.forEach(value=>{
+//     console.log(value)
+// })
+
+// 2.. using for of loop 
+
+// for(let item of myset){
+//     console.log(item)
+// }
